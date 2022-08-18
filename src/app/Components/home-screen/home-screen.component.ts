2563e971eee164838d824da88e74cdf09aabb268
Yordan Nikolov <jnikolow93@gmail.com>
2022-08-18 20:04:18 +0100

    refactoring code

--- src/app/Components/home-screen/home-screen.component.ts
+++ src/app/Components/home-screen/home-screen.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// import { style } from './home-screen.component.css'
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.css']
+  styleUrls: ['./home-screen.component.css'],
+
 })
 export class HomeScreenComponent implements OnInit {
 
