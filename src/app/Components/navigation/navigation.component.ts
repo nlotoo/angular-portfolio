481ebcd30ce9f79e15b887fa90032bd7a84917ee
Yordan Nikolov <jnikolow93@gmail.com>
2022-08-19 08:01:05 +0100

    navigation menu refactorated

--- src/app/Components/navigation/navigation.component.ts
+++ src/app/Components/navigation/navigation.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
+  //   console.log('sled towa')
+  // }
+
+  ActiveNav = ''
+  ActiveNavAbout = ''
+  ActiveNavExperience = ''
+  ActiveNavPortfolio = ''
+  ActiveNavContact = ''
+
+  setACtiveNav(data: string) {
+
+
+    if (data === '#') {
+      this.ActiveNav = 'active-a-tag'
+      this.ActiveNavExperience = ''
+      this.ActiveNavPortfolio = ''
+      this.ActiveNavAbout = ''
+      this.ActiveNavContact = ''
+
+    }
+    else if (data === '#about') {
+      this.ActiveNavAbout = 'active-a-tag'
+      this.ActiveNavExperience = ''
+      this.ActiveNavPortfolio = ''
+      this.ActiveNav = ''
+      this.ActiveNavContact = ''
+
+    }
+    else if (data === '#experience') {
+      this.ActiveNavExperience = 'active-a-tag'
+      this.ActiveNavAbout = ''
+      this.ActiveNavPortfolio = ''
+      this.ActiveNav = ''
+      this.ActiveNavContact = ''
+
+    }
+    else if (data === '#portfolio') {
+      this.ActiveNavPortfolio = 'active-a-tag'
+      this.ActiveNav = ''
+      this.ActiveNavExperience = ''
+      this.ActiveNavAbout = ''
+      this.ActiveNavContact = ''
+
+    }
+    else if (data === '#contact') {
+      this.ActiveNavContact = 'active-a-tag'
+      this.ActiveNav = ''
+      this.ActiveNavExperience = ''
+      this.ActiveNavAbout = ''
+      this.ActiveNavPortfolio = ''
+    }
+
+    // this.ActiveNav === '#' ? 'active-a-tag' : ''
+  }
+
+
+
+  ngOnChanges() {
+
   }
 
 }
