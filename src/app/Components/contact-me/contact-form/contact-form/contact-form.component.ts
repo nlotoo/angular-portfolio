0c95e8864d41a10de5b59c0d5433916074b64cf6
Yordan Nikolov <jnikolow93@gmail.com>
2022-08-21 08:17:00 +0100

    contact form updated email js integated and setup

--- src/app/Components/contact-me/contact-form/contact-form/contact-form.component.ts
+++ src/app/Components/contact-me/contact-form/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ContactFormComponent implements OnInit {
 
   constructor() { }
+
+  public sendEmail(e: Event) {
+    
+    e.preventDefault();
+    emailjs.sendForm('service_hnrbu7e', 'template_67bv6wt', e.target as HTMLFormElement, 'iOJVDuyDZk3gqt1zo')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+  }
+  
 
   ngOnInit(): void {
   }
